9a3bebce9555df25fd178266719aff57d32390d9
LariLeal <morolari13@gmail.com>
2025-08-19 07:51:07 -0300

    Montando a primeira tela inicial, colocando cores e arrumando estilizacao da tela

---
+++ src/global/themes.tsx
@@ -0,0 +1,9 @@
+export const themas = {
+    colors: {
+        primary: '#878af6',
+        seconsary: '#ffffff',
+        lightGray: '#d7d8d7',
+        gray: 'gray',
+        bigScreen: '#f1f7fa'
+    }
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,14 +8,20 @@ export default function Login() {
         <View style={style.container}>
             <View style={style.boxTop}>
                 <Image
-                source={Logo}
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
                 />
-                <Text>Bem vindo de volta!</Text>
+                <Text style={style.text}>Bem vindo de volta</Text>
             </View>
             <View style={style.boxMid} >
-                <Text>Endereço de E-mail</Text>
-                <TextInput />
-                <Text>Senha</Text>
+                <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+                <View style={style.boxInput}>
+                    <TextInput
+                    style={style.input}
+                    />
+                </View>
+                <Text style={style.titleInput}>SENHA</Text>
                 <TextInput />
             </View>
             <View style={style.boxBottom}>
