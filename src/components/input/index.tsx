4a458513fca440ac121c882d3efee84847c08927
LariLeal <morolari13@gmail.com>
2025-09-01 15:10:29 -0300

    Realizando alteracoes, para ajustes mobile, colocando components e passando propriedades atribuidas

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef , Fragment} from "react";
+import React, { forwardRef , Fragment, LegacyRef } from "react";
 import {View, Text, TextInput, TextInputProps} from 'react-native';
 import {style} from "./style";
 // import {MaterialIcons} from '@expo/vector-icons';
@@ -12,17 +12,20 @@ type IconComponent = React.ComponentType<React.ComponentProps< typeof MaterialIc
     type Props = TextInputProps & {
         IconLeft?: IconComponent,
         IconRight?: IconComponent,
-        IconLeftname?: string,
+        IconLeftName?: string,
         IconRightName ?: string,
         title ?: string,
-        OnIconleftPress ?: () => void,
+        OnIconLeftPress ?: () => void,
         OnIconRightPress ?: () => void
     }
 
-export const Input = forwardRef(() => {
+export const Input = forwardRef((Props: Props, forwardRef: LegacyRef<TextInput> | null) => {
+    const {IconLeft, IconRight, IconLeftName, IconRightName, title, OnIconLeftPress, OnIconRightPress,
+        ...rest
+    } = Props
     return (
         <Fragment>
-            <Text style={ style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+            <Text style={ style.titleInput}>{title}</Text>
             <View style={style.boxInput}>
                 <TextInput
                     style={style.input}
