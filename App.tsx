e1e090836d45d224a85c34f11281d3070b5cff19
LARISSA LEAL MORO <larissa.1965@aluno.pr.senac.br>
2025-09-08 15:04:18 -0300

    Componentizando toda a tela, componentizando o botao e tambem arrumando a estrutura

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './src/pages/login';
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login />
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +15,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+
+import { Text, View, Image, TextInput, TouchableOpacity, Alert, ActivityIndicator} from "react-native";
+import { style } from "./styles";
+import Logo from "../../assets/logo.png";
+import {MaterialIcons, Octicons} from '@expo/vector-icons';
+import {themas} from "../../global/themes";
+import {Input} from "../../components/input";
+import {Button} from "../../components/Button";
+ 
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    async function getLogin() {
+        try{
+            setLoading(true)
+            if (!email || !password) {
+                return Alert.alert('Atenção', 'Informe os campos obrigatórios!');
+            }
+
+            setTimeout(() => {
+                if (email == 'lari@gmail.com' && password == '12345678') {
+                    Alert.alert('Logado com sucesso!');
+                } else {
+                    Alert.alert('Usuário não encontrado!');
+                }
+                setLoading(false)
+            }, 3000);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Image
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo de volta!</Text>
+            </View>
+            <View style={style.boxMid} >
+                <Input 
+                value={email}
+                onChangeText={setEmail}
+                title="ENDEREÇO E-MAIL"
+                IconRight={MaterialIcons}
+                IconRightName="email"
+                />
+                <Input
+                value={password}
+                onChangeText={setPassword}
+                title="SENHA"
+                IconRight={Octicons}
+                IconRightName={showPassword ? "eye-closed" : "eye"}
+                secureTextEntry={showPassword}
+                onIconRightPress={() => setShowPassword(!showPassword)}
+                />
+                
+            </View>
+            <View style={style.boxBottom}>
+                <Button text  = "Entrar" loading = {loading} onPress={() => getLogin()} />
+            </View>
+            <Text style={style.textBotton}>Não tem conta?
+            <Text style={{color: themas.colors.primary}}> Crie agora!</Text></Text>
+        </View>
+    )
+}
