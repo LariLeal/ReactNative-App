d13f2f652cf20d6330ac664d6bd53b1120d33c1c
LARISSA LEAL MORO <larissa.1965@aluno.pr.senac.br>
2025-09-29 09:01:59 -0300

    Criando constantes que usaremos para os proximos passos da programaçao, alem de deixar o teclado responsivo quando abrir em qualquer aparelho celular

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,5 +1,5 @@
-import React, {createContext, useContext, useRef, useEffect} from "react";
-import { Dimensions, Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import React, {createContext, useContext, useRef, useEffect, useState} from "react";
+import { Dimensions, Text, View, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 import {MaterialIcons, AntDesign} from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
@@ -15,7 +15,12 @@ const flags = [
 
 export const AuthProviderList = (props: any): any => {
     const modalizeRef = useRef<Modalize>(null);
-    
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedFlag, setSelected] = useState('Urgente');
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedTime, setSelectedTime] = useState(new Date());
+
     const onOpen = () => {
         modalizeRef?.current?.open();
     }
@@ -44,7 +49,10 @@ export const AuthProviderList = (props: any): any => {
 
     const _container = () => {
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
                 <View style={styles.header}>
                     <TouchableOpacity onPress={() => onClose()}>
                         <MaterialIcons 
@@ -67,6 +75,8 @@ export const AuthProviderList = (props: any): any => {
                     <Input 
                         title="Título"
                         labelStyle={styles.label}
+                        value={title}
+                        onChangeText={setTitle}
                     />
                     <Input
                         title="Descrição"
@@ -74,6 +84,8 @@ export const AuthProviderList = (props: any): any => {
                         height={100}
                         multiline
                         numberOfLines={5}
+                        value={description}
+                        onChangeText={setDescription}
                     />
                 </View>
                 <View style={{width: '40%'}}>
@@ -88,7 +100,7 @@ export const AuthProviderList = (props: any): any => {
                         {_renderFlags()}
                     </View>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         )
     }
     return (
